6ea3b095f719c70304efc366c008e8fdc766e0da
Artem Vovchenko <a.vovchenko01@gmail.com>
2021-07-27 21:40:07 +0300

    Add sway, awesome and zsh configs

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,5 @@
+packadd minpac
+call minpac#init()
+call minpac#add('vimwiki/vimwiki')
+call minpac#add('k-takata/minpac',{'type':'opt'})
+
--- scripts/software.sh
+++ scripts/software.sh
@@ -6,18 +6,29 @@ function ask {
 
 key=Y
 
-pacman-mirrors -f
-pacman -Syu
+# pacman-mirrors -f
+sudo pacman -Sy
 
 soft=""
 
-for prog in $(cat software_list.txt); do
-	ask "Install ${prog}?"
-	if [ ! $key = 'N' ]; then 
-		soft="${soft} ${prog}"
-		echo "Progs: ${soft}"
-	fi
-done
+echo "Comment lines which you don't need ..."
+sleep 2
+vim software_list.txt
+#for prog in $(cat software_list_temp.txt); do
+#	ask "Install ${prog}?"
+#	if [ ! $key = 'N' ]; then 
+#	soft="${soft} ${prog}"
+#		echo "Progs: ${soft}"
+#	fi
+#done
 
-pacman -S ${soft}
+if ! pacman -Q | grep -q paru; then
+	echo "Installing paru ..."
+	git clone https://aur.archlinux.org/paru.git
+	cd paru
+	makepkg -si
+	cd ..
+	rm -rf paru
+fi
+paru -S $(cat software_list.txt | grep -v '#')
 
