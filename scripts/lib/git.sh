7d916d9de095018a52e324071da055fd4e515679
Artem Vovchenko <a.vovchenko01@gmail.com>
2022-09-01 20:58:55 +0300

    Updates

--- scripts/lib/git.sh
+++ scripts/lib/git.sh
@@ -14,18 +14,24 @@ function my_os_lib_git_commit {
 function my_os_lib_git_get_url {
   case $1 in
     browser-profiles)
+      echo "https://github.com/artem-vovchenko01/browser-profiles.git"
       ;;
     vimwiki)
+      echo "https://github.com/artem-vovchenko01/vimwiki.git"
       ;;
     linux-files)
+      echo "https://github.com/artem-vovchenko01/linux-files.git"
       ;;
   esac
 }
 
 function my_os_lib_git_select {
   url=$(my_os_lib_git_get_url $1)
-  [[ -e $MY_OS_PATH_GIT/"$1" ]] || 
-  lib dir }
+  lib dir $MY_OS_PATH_GIT
+  [[ -e $MY_OS_PATH_GIT/"$1" ]] || lib run "git clone $url"
+  lib dir "$1"
+  lib log "lib git: repository $1 is selected"
+}
 
 function my_os_lib_git_check_updates {
 lib log "Checking if there are update is $(pwd) repo"
