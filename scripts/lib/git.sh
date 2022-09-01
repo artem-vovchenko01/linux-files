ac68718d1cf257448067c8c8824e6172d1f85066
Artem Vovchenko <a.vovchenko01@gmail.com>
2022-09-01 23:59:04 +0300

    Updates

--- scripts/lib/git.sh
+++ scripts/lib/git.sh
@@ -1,3 +1,9 @@
+MY_OS_GIT_ARTIFACT_NAME=archive.zip
+MY_OS_GIT_PATH_FIREFOX_PROFILES="~/.mozilla/firefox"
+MY_OS_GIT_PATH_SOFTWARE_BACKUPS=$MY_OS_PATH_BASE
+MY_OS_GIT_ARTIFACT_DIR_BROWSER_PROFILES=3fl1hnwj.default-release
+MY_OS_GIT_ARTIFACT_DIR_SOFTWARE_BACKUPS=software-backups
+
 function my_os_lib_git_commit {
   lib log "git status"
   git status
@@ -22,7 +28,60 @@ function my_os_lib_git_get_url {
     linux-files)
       echo "https://github.com/artem-vovchenko01/linux-files.git"
       ;;
+    software-backups)
+      echo "https://github.com/artem-vovchenko01/software-backups.git"
+      ;;
   esac
+}
+
+function my_os_lib_git_get_artifact_destination {
+  case "$(lib git get-selected-repo)" in
+    browser-profiles)
+      echo "$MY_OS_GIT_PATH_FIREFOX_PROFILES"
+      ;;
+    software-backups)
+      echo "$MY_OS_GIT_PATH_SOFTWARE_BACKUPS"
+      ;;
+  esac
+}
+
+function my_os_lib_git_get_artifact_dir_name {
+  case "$(lib git get-selected-repo)" in
+    browser-profiles)
+      echo "$MY_OS_GIT_ARTIFACT_DIR_BROWSER_PROFILES"
+      ;;
+    software-backups)
+      echo "$MY_OS_GIT_ARTIFACT_DIR_SOFTWARE_BACKUPS"
+      ;;
+  esac
+}
+
+function my_os_lib_git_artifact_unpack {
+  lib log "Trying to unpack artifact from $(lib git get-selected-repo)"
+  name_of_extracted_dir=$(zip -sf $MY_OS_GIT_ARTIFACT_NAME | grep -E '.*/$' | head -n1 | tr -d ' ' | tr -d '/')
+  artifact_destination="$(my_os_lib_git_get_artifact_destination)"
+  ls "${artifact_destination}/${name_of_extracted_dir}" &> /dev/null && {
+    lib log warn "Destination ${artifact_destination}/{name_of_extracted_dir} already exists. Won't proceed"
+    return
+  }
+  lib run "unzip $MY_OS_GIT_ARTIFACT_NAME -d $artifact_destination"
+}
+
+function my_os_lib_git_archive_artifact {
+  lib run "mkdir -p ARCH"
+  lib run "zip -re $1.zip "
+}
+
+function my_os_lib_git_force_push_artifact {
+  REMOTE_URL=$(git remote show origin -n | tr ' ' '\n' | grep https | head -n 1)
+  lib run "rm -rf .git"
+  lib run "rm -f $MY_OS_GIT_ARTIFACT_NAME"
+  lib run "git init"
+  lib run "git remote add origin $REMOTE_URL"
+  lib run "cp -r $(my_os_lib_git_get_artifact_destination)/$(my_os_lib_git_get_artifact_dir_name) ."
+  lib run "zip -re $MY_OS_GIT_ARTIFACT_NAME $(my_os_lib_git_get_artifact_dir_name)"
+
+  lib run "git push -f --set-upstream origin main"
 }
 
 function my_os_lib_git_select {
@@ -30,7 +89,12 @@ function my_os_lib_git_select {
   lib dir $MY_OS_PATH_GIT
   [[ -e $MY_OS_PATH_GIT/"$1" ]] || lib run "git clone $url"
   lib dir "$1"
-  lib log "lib git: repository $1 is selected"
+  MY_OS_GIT_SELECTED_REPO="$1"
+  lib log "lib git: repository $MY_OS_GIT_SELECTED_REPO is selected"
+}
+
+function my_os_lib_git_get_selected_repo {
+  echo "$MY_OS_GIT_SELECTED_REPO"
 }
 
 function my_os_lib_git_check_updates {
@@ -45,4 +109,58 @@ lib log "Checking if there are update is $(pwd) repo"
       return 1;
   fi
 }
+
+
+commit() {
+  log "git status"
+  git status
+  if ! yes-or-no "Are you satisfied with the status?"; then
+    exit 1
+  fi
+
+  log "git add"
+  git add .
+  log "git commit"
+  git commit -m "Commit at $(date)"
+}
+
+check-updates() {
+log "Checking if there are update is $(pwd) repo"
+  lines=$(git status -s | wc -l)
+  if [[ $lines -gt 0 ]]; then
+      log "There are some updates in this repo!!! Comitting them ..."
+      commit
+      return 0;
+    else
+      log "There are no updates in this repo"
+      return 1;
+  fi
+}
+
+company() {
+  archive_name=$(date | tr ' ' '-').zip
+  cd ./COMPANY
+
+  if check-updates; then
+      log "Zipping COMPANY... "
+      cd ..
+      zip -re ./ARCHIVES/$archive_name COMPANY/
+      cd COMPANY/
+    else
+      log "COMPANY have no updates, so no zip archive is created. "
+  fi
+
+  cd ..
+}
+
+# WORKING ON COMPANY NOTES
+
+company
+
+# COMITTING GENERAL NOTES
+
+if check-updates; then
+  log "git push"
+  git push
+fi
 
