9301cce5139d9d79768faf6ddb09543656aad010
Artem Vovchenko <a.vovchenko01@gmail.com>
2024-08-28 18:13:31 +0300

    Update configs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-mkdir -p ~/Custom_setup
+mkdir -p ~/custom-setup/hyprland/{pomodoro,state}
 
-ln -s ~/linux-files/scripts ~/Custom_setup/scripts
+############################################
+# CONFIGS
+############################################
+
+# Hyprland
+mkdir -p ~/.config/hypr
+ln -sf ~/linux-files/dotfiles/hyprland/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf ~/linux-files/dotfiles/hyprland/hypridle.conf ~/.config/hypr/hypridle.conf
+ln -sf ~/linux-files/dotfiles/hyprland/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+
+# Mako
+mkdir -p ~/.config/mako
+ln -sf ~/linux-files/dotfiles/mako/.config/mako/config ~/.config/mako/config
+
+# Lunarvim
+mkdir -p ~/.config/lvim
+ln -sf ~/linux-files/dotfiles/lunarvim/.config/lvim/config.lua ~/.config/lvim/config.lua
