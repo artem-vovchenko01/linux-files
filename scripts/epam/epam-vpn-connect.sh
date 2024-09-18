9ac242002c2ab690a66d0354302fe6ce1001f906
Artem Vovchenko <artem_vovchenko@epam.com>
2024-09-18 10:24:35 +0300

    Add small updates

--- scripts/epam/epam-vpn-connect.sh
+++ scripts/epam/epam-vpn-connect.sh
@@ -1,2 +1,2 @@
-# sudo gpclient connect vpn-ua.epam.com
-sudo gpclient connect vpn-eu.epam.com
+# sudo gpclient connect vpn-eu.epam.com
+sudo gpclient connect vpn-ua.epam.com
--- scripts/tool/laptop/laptop-route-pc.sh
+++ scripts/tool/laptop/laptop-route-pc.sh
@@ -1,3 +1,5 @@
+sudo sysctl -w  net.ipv4.ip_forward=1
+
 sudo iptables -t nat -A POSTROUTING -d 0/0 -s 192.168.0/24 -j MASQUERADE
 
 sudo iptables -A FORWARD -s 192.168.0.0/24 -d 0/0 -j ACCEPT
