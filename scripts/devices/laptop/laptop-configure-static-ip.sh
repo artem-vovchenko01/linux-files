9798903713040d871f96847e3be63d80a895d498
Artem Vovchenko <artem_vovchenko@epam.com>
2024-09-19 15:54:52 +0300

    Restructure files

---
+++ scripts/devices/laptop/laptop-configure-static-ip.sh
@@ -0,0 +1,4 @@
+nmcli c modify TP-Link_0EBC_5G ipv4.addresses 192.168.0.182/24
+nmcli c modify TP-Link_0EBC_5G ipv4.gateway 192.168.0.1
+nmcli c modify TP-Link_0EBC_5G ipv4.dns 192.168.0.1
+nmcli c modify TP-Link_0EBC_5G ipv4.method manual 
