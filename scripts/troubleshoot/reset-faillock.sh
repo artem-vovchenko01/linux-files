9be2cdf9b12758cadbabeec40ca5b08fc87a6748
Artem Vovchenko <artem_vovchenko@epam.com>
2026-03-10 18:46:54 +0100

    feat: account for different usernames

--- scripts/troubleshoot/reset-faillock.sh
+++ scripts/troubleshoot/reset-faillock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kitty bash -c "echo -e 'faillock --user artem\nshow failed attempts\nfaillock --user artem --reset\nreset'; su"
+kitty bash -c "echo -e 'faillock --user $USER\nshow failed attempts\nfaillock --user $USER --reset\nreset'; su"
